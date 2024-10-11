f3af03e781ff165417605db45c2af88269a31e12
Thomas Kluyver <thomas@kluyver.me.uk>
2024-10-11 21:59:04 +0200

    Add demo using QNetwork

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
-from PyQt5 import QtCore, QtWidgets, QtGui
+import time
+
+from PyQt5 import QtCore, QtNetwork, QtWidgets, QtGui
 
 from qt_await import (
-    connect_async, start_async, with_timeout, sleep_loop, run_process
+    connect_async, start_async, with_timeout, sleep_loop, run_process, SignalQueue
 )
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -22,6 +24,10 @@ class MainWindow(QtWidgets.QMainWindow):
         buttons_vbox.addWidget(button_proc)
         connect_async(button_proc.clicked, self.run_process)
 
+        button_http = QtWidgets.QPushButton("HTTP request")
+        buttons_vbox.addWidget(button_http)
+        connect_async(button_http.clicked, self.http_request)
+
         self.counter_sb = QtWidgets.QSpinBox()
         start_async(self.count_up())
         buttons_vbox.addWidget(self.counter_sb)
@@ -34,19 +40,33 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def write_output(self, text):
         self.write_cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
-        self.write_cursor.insertText(text)
+        self.write_cursor.insertText(text + '\n')
         vsb = self.output.verticalScrollBar()
         vsb.setValue(vsb.maximum())  # Scroll to end
 
     async def run_process(self):
         proc = QtCore.QProcess(self)
         await run_process(proc, "date", [])
-        out_s = bytes(proc.readAllStandardOutput()).decode()
+        out_s = bytes(proc.readAllStandardOutput()).decode().strip()
         self.write_output(f"date output: {out_s}")
 
         proc = QtCore.QProcess(self)
         ec, _ = await with_timeout(run_process(proc, "sleep", ["2"]), 5000)
         self.write_output(f"sleep exited with {ec}")
+
+    async def http_request(self):
+        mgr = QtNetwork.QNetworkAccessManager()
+        req = QtNetwork.QNetworkRequest(QtCore.QUrl("https://pypi.org/"))
+        reply = mgr.get(req)
+
+        t0 = time.perf_counter()
+        await SignalQueue(reply.finished)
+        t1 = time.perf_counter()
+
+        status = reply.attribute(QtNetwork.QNetworkRequest.Attribute.HttpStatusCodeAttribute)
+        content_type = reply.header(QtNetwork.QNetworkRequest.KnownHeaders.ContentTypeHeader)
+        self.write_output(f"HTTP response {status} in {t1 - t0:.3f}s")
+        self.write_output(f"  mime type {content_type}")
 
     async def count_up(self):
         async for _ in sleep_loop(1000):
@@ -56,5 +76,6 @@ class MainWindow(QtWidgets.QMainWindow):
 if __name__=="__main__":
     qapp = QtWidgets.QApplication([])
     window = MainWindow()
+    window.resize(700, 400)
     window.show()
     qapp.exec()
