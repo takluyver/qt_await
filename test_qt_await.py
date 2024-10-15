6a49e0204d682db362a0716d661f3e6947fc415f
Thomas Kluyver <thomas@kluyver.me.uk>
2024-10-15 22:23:32 +0200

    More tests

--- test_qt_await.py
+++ test_qt_await.py
@@ -1,13 +1,15 @@
+import sys
 import time
 
 import pytest
 from pytestqt.qt_compat import qt_api
+QtCore = qt_api.QtCore
 
-from qt_await import start_async, sleep
+from qt_await import start_async, sleep, with_timeout, run_process, read_streaming_text
 
 @pytest.fixture(scope="session")
 def qapp_cls():
-   return qt_api.QtCore.QCoreApplication
+   return QtCore.QCoreApplication
 
 def test_sleep(qtbot):
     async def sleep_eg(cb):
@@ -21,3 +23,67 @@ def test_sleep(qtbot):
 
     assert 0.4 < cb.args[0] < 0.6
 
+
+def test_with_timeout(qtbot):
+    async def timeout_eg_1(cb):
+        t0 = time.perf_counter()
+        try:
+            await with_timeout(sleep(ms=200), timeout_ms=500)
+            err = False
+        except TimeoutError as e:
+            err = True
+        t1 = time.perf_counter()
+        cb(err, t1 - t0)
+
+    with qtbot.waitCallback(timeout=2000) as cb:
+        start_async(timeout_eg_1(cb))
+
+    assert cb.args[0] is False
+    assert 0.1 < cb.args[1] < 0.3
+
+    async def timeout_eg_2(cb):
+        t0 = time.perf_counter()
+        try:
+            await with_timeout(sleep(ms=500), timeout_ms=200)
+            err = False
+        except TimeoutError as e:
+            err = True
+        t1 = time.perf_counter()
+        cb(err, t1 - t0)
+
+    with qtbot.waitCallback(timeout=2000) as cb:
+        start_async(timeout_eg_2(cb))
+
+    assert cb.args[0] is True
+    assert 0.1 < cb.args[1] < 0.3
+
+
+def test_run_process(qtbot):
+    async def proc_eg(cb):
+        qp = QtCore.QProcess()
+        await run_process(qp, sys.executable, ['-c', 'print(6 * 7)'])
+        cb(bytes(qp.readAllStandardOutput()))
+
+    with qtbot.waitCallback(timeout=2000) as cb:
+        start_async(proc_eg(cb))
+
+    assert cb.args[0].strip() == b'42'
+
+
+def test_read_streaming_text(qtbot, qapp):
+    async def streaming_eg(cb):
+        qp = QtCore.QProcess(qapp)
+        qp.setProcessChannelMode(QtCore.QProcess.ForwardedErrorChannel)
+        qp.start(sys.executable)
+        qp.write(b'import time\nfor i in range(5):\n  print(i, flush=True)\n  time.sleep(0.1)')
+        qp.closeWriteChannel()
+        pieces = []
+        async for s in read_streaming_text(qp):
+            pieces.append(s.strip())
+        qp.deleteLater()
+        cb(pieces)
+
+    with qtbot.waitCallback(timeout=2000) as cb:
+        start_async(streaming_eg(cb))
+
+    assert cb.args[0] == ['0', '1', '2', '3', '4']
